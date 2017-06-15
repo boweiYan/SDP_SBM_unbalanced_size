832e3c7cb804333d449f8b7ccdaae4783f4cf47b
boweiYan <yanbowei@gmail.com>
2017-06-14 19:32:51 -0500

    v 1.1
    update cl2mat

--- cl2mat.m
+++ cl2mat.m
@@ -5,12 +5,13 @@ function mat = cl2mat(cl)
 % Output: mat: n*k binary matrix
 
 % Author: Bowei Yan
-% Last modified: June 13, 2017
+% Last modified: June 14, 2017
 
 n = length(cl);
-mat = zeros(n);
-for i = 1:n,
-    for j = 1:n,
-        mat(i,j) = 1*(cl(i)==cl(j));
-    end
+k = length(unique(cl));
+mat = zeros(n,k);
+
+for j = 1:k,
+    mat(:,j) = 1*(cl==j);
 end
+
